6763d96fe6ef59410fca85c905c61f520f142394
junreturn1982 <ngochoang071982@gmail.com>
2017-06-14 20:56:15 +0700

    http

--- _Pluralsight/BuildingWebAppWithNodeJSAndExpress/073rdpartyapi/src/services/goodreadsService.js
+++ _Pluralsight/BuildingWebAppWithNodeJSAndExpress/073rdpartyapi/src/services/goodreadsService.js
@@ -1,6 +1,28 @@
+const http = require('http');
+const xml2js = require('xml2js');
+const parser = xml2js.Parser({explicitArray: false});
+
 const goodreadsService = function () {
+
     let getBookById = (id, cb) => {
-        cb(null, {description: "Our description"});
+        let options = {
+            host: 'www.goodreads.com',
+            path: '/book/show/656.xml?key=ucd8Ov2Te2rCOBhTabFhw'
+        };
+        let callback = (response) => {
+            let str = '';
+
+            response.on('data', (chunk) => {
+                str += chunk;
+            });
+            response.on('end', () => {
+                console.log(str);
+                parser.parseString(str, (err, result) => {
+                    cb(null, result.GoodreadsResponse.book);
+                });
+            });            
+        };
+        http.request(options, callback).end();
     };
 
     return {
